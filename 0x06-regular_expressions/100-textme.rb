eb55821a17e0c4344e58858777bc33e98c7759a8
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-08-01 11:32:27 -0700

    some permmisions given

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/env ruby
-# REGEX that matches A-given pattern
-puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
